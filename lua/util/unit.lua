dbd701239ec41d5a828e10bbf8fe5d5418f1dfd7
Alexander Ames <alexander.ames@gmail.com>
2023-10-13 11:21:23 -0700

    More improvements to error reporting

--- lua/util/unit.lua
+++ lua/util/unit.lua
@@ -34,12 +34,21 @@ local function table_to_string(t)
   return s .. '}'
 end
 
+local function collect_keys(out, ...)
+  for i, t in pairs{...} do
+    for k, v in pairs(t) do
+      out[k] = true
+    end
+  end
+  return out
+end
+
 --------------------------------------------------------------------------------
-local fmt = 'expected that\n  %s\n%s\n  %s'
+local fmt = 'expected\n  %s\nto %s\n  %s'
 function EXPECT_THAT(actual, predicate)
-  result, a, b, c = predicate(actual, false)
+  result, act, msg, nmsg, exp = predicate(actual, false)
   if not result then
-    error(fmt:format(a, b, c))
+    error(fmt:format(act, msg, exp))
   end
 end
 
@@ -61,55 +70,107 @@ end
 
 function Not(predicate)
   return function(actual)
-    local result, act, msg, exp = predicate(actual)
-    return not result, act, 'not ' .. msg, exp
+    local result, act, msg, nmsg, exp = predicate(actual)
+    return not result, act, nmsg, msg, exp
   end
 end
 
 function Equals(expected)
   return function(actual)
-    local result = actual == expected
+    local result = (actual == expected)
     return
       result,
       tostring(actual),
       'be equal to',
+      'be not equal to',
+      tostring(expected)
+  end
+end
+
+function GreaterThan(expected)
+  return function(actual)
+    local result = (actual > expected)
+    return
+      result,
+      tostring(actual),
+      'be greater than',
+      'be not greater than',
+      tostring(expected)
+  end
+end
+
+function GreaterThanOrEqual(expected)
+  return function(actual)
+    local result = (actual >= expected)
+    return
+      result,
+      tostring(actual),
+      'be greater than or equal to',
+      'be not greater than or equal to',
+      tostring(expected)
+  end
+end
+
+function LessThan(expected)
+  return function(actual)
+    local result = (actual < expected)
+    return
+      result,
+      tostring(actual),
+      'be less than',
+      'be not less than',
+      tostring(expected)
+  end
+end
+
+function LessThanOrEqual(expected)
+  return function(actual)
+    local result = (actual <= expected)
+    return
+      result,
+      tostring(actual),
+      'be less than or equal to',
+      'be not less than or equal to',
       tostring(expected)
   end
 end
 
 function Listwise(predicate_generator, expected)
   return function(actual)
-    local result, act, msg, exp
+    local result = true
+    local act, msg, nmsg, exp
+    local act_list, exp_list = {}, {}
     for i=1, max(#actual, #expected) do
-      local predicate = predicate_generator(actual[i])
-      result, act, msg, exp = predicate(expected[i])
-      if not result then
-        break
-      end
+      local predicate = predicate_generator(expected[i])
+      local local_result
+      local_result, act, msg, nmsg, exp = predicate(actual[i])
+      act_list[i], exp_list[i] = act, exp
+      result = result and local_result
     end
     return result,
-           list_to_string(actual),
-           msg .. ' at every index to',
-           list_to_string(expected)
+           list_to_string(act_list),
+           msg .. ' the value at every index of',
+           'not to ' .. msg .. ' the value at every index of',
+           list_to_string(exp_list)
   end
 end
 
 function Tablewise(predicate_generator, expected)
   return function(actual)
-    local result, act, msg, exp
-    local keys = {}
-    for k, _ in pairs(actual) do keys[k] = true end
-    for k, _ in pairs(expected) do keys[k] = true end
+    local result = true
+    local act, msg, nmsg, exp
+    local act_list, exp_list = {}, {}
+    local keys = collect_keys({}, actual, expected)
     for k, _ in pairs(keys) do
-      local predicate = predicate_generator(actual[k])
-      result, act, msg, exp = predicate(expected[k])
-      if not result then
-        break
-      end
+      local predicate = predicate_generator(expected[k])
+      local local_result
+      local_result, act, msg, nmsg, exp = predicate(actual[k])
+      result = result and local_result
     end
     return result,
            table_to_string(actual),
-           msg .. ' at every index to',
+           msg .. ' the value at every key of',
+           'not to ' .. msg .. ' the value at every key of',
            table_to_string(expected)
   end
 end
@@ -178,44 +239,54 @@ function test()
   EXPECT_NE({1, 2, 3}, {2, 3})
   EXPECT_NE({1, 2}, {2, 3})
 
-  -- -- EXPECT_THAT({a=100, b="hello"}, Listwise(Equals({a=100, b="hello"}))
   EXPECT_THAT({1, 2, 3}, Listwise(Equals, {1, 2, 3}))
   EXPECT_THAT({1, 2, 3}, Listwise(function(v) return Not(Equals(v)) end, {2, 4, 6}))
   EXPECT_THAT({1, 2, 3}, Not(Listwise(Equals, {1, 2, 4})))
-  -- EXPECT_THAT({1, 2, 3}, Not(Listwise(function(v) return Not(Equals(v)) end), {1, 2, 3}))
-  -- EXPECT_THAT({1, 2, 3}, Not(Listwise(function(v) return Not(Equals(v)) end), {1, 2, 4}))
+  EXPECT_THAT({1, 2, 3}, Not(Listwise(function(v) return Not(Equals(v)) end, {1, 2, 3})))
+  EXPECT_THAT({1, 2, 3}, Not(Listwise(function(v) return Not(Equals(v)) end, {1, 2, 4})))
 
   EXPECT_THAT({a=100, b="hello"}, Tablewise(Equals, {a=100, b="hello"}))
   EXPECT_THAT({a=100, b="hello"}, Tablewise(function(v) return Not(Equals(v)) end, {a=1000, b="goodbye"}))
-  -- EXPECT_THAT({a=100, b="hello"}, Not(Tablewise(Equals), {a=100, b="hello", c="world"}))
-  -- EXPECT_THAT({a=100, b="hello", c="world"}, Not(Tablewise(Equals), {a=100, b="hello"}))
+  EXPECT_THAT({a=100, b="hello"}, Not(Tablewise(Equals, {a=100, b="hello", c="world"})))
+  EXPECT_THAT({a=100, b="hello", c="world"}, Not(Tablewise(Equals, {a=100, b="hello"})))
 
   -- -- Test to ensure they fail when they get bad values
-  print(pcall(EXPECT_TRUE, false))
-  print(pcall(EXPECT_FALSE, true))
+  EXPECT_FALSE(pcall(EXPECT_TRUE, false))
+  EXPECT_FALSE(pcall(EXPECT_FALSE, true))
 
-  print(pcall(EXPECT_EQ, nil, 1))
-  print(pcall(EXPECT_EQ, true, false))
-  print(pcall(EXPECT_EQ, nil, 1))
-  print(pcall(EXPECT_EQ, false, true))
-  print(pcall(EXPECT_EQ, 1, 2))
-  print(pcall(EXPECT_EQ, "hello", "world"))
-  print(pcall(EXPECT_EQ, next, print))
-  print(pcall(EXPECT_EQ, {a=100, b="hello"}, {a=100}))
-  print(pcall(EXPECT_EQ, {b="hello"}, {a=100, b="hello"}))
-  print(pcall(EXPECT_EQ, {a=100, b="hello"}, {c=100, d="hello"}))
-  print(pcall(EXPECT_EQ, {1, 2}, {1, 2, 3}))
-  print(pcall(EXPECT_EQ, {1, 2, 3}, {2, 3}))
-  print(pcall(EXPECT_EQ, {1, 2}, {2, 3}))
+  EXPECT_FALSE(pcall(EXPECT_EQ, nil, 1))
+  EXPECT_FALSE(pcall(EXPECT_EQ, true, false))
+  EXPECT_FALSE(pcall(EXPECT_EQ, nil, 1))
+  EXPECT_FALSE(pcall(EXPECT_EQ, false, true))
+  EXPECT_FALSE(pcall(EXPECT_EQ, 1, 2))
+  EXPECT_FALSE(pcall(EXPECT_EQ, "hello", "world"))
+  EXPECT_FALSE(pcall(EXPECT_EQ, next, print))
+  EXPECT_FALSE(pcall(EXPECT_EQ, {a=100, b="hello"}, {a=100}))
+  EXPECT_FALSE(pcall(EXPECT_EQ, {b="hello"}, {a=100, b="hello"}))
+  EXPECT_FALSE(pcall(EXPECT_EQ, {a=100, b="hello"}, {c=100, d="hello"}))
+  EXPECT_FALSE(pcall(EXPECT_EQ, {1, 2}, {1, 2, 3}))
+  EXPECT_FALSE(pcall(EXPECT_EQ, {1, 2, 3}, {2, 3}))
+  EXPECT_FALSE(pcall(EXPECT_EQ, {1, 2}, {2, 3}))
 
-  print(pcall(EXPECT_NE, nil, nil))
-  print(pcall(EXPECT_NE, true, true))
-  print(pcall(EXPECT_NE, false, false))
-  print(pcall(EXPECT_NE, 1, 1))
-  print(pcall(EXPECT_NE, "hello, world", "hello, world"))
-  print(pcall(EXPECT_NE, next, next))
+  EXPECT_FALSE(pcall(EXPECT_NE, nil, nil))
+  EXPECT_FALSE(pcall(EXPECT_NE, true, true))
+  EXPECT_FALSE(pcall(EXPECT_NE, false, false))
+  EXPECT_FALSE(pcall(EXPECT_NE, 1, 1))
+  EXPECT_FALSE(pcall(EXPECT_NE, "hello, world", "hello, world"))
+  EXPECT_FALSE(pcall(EXPECT_NE, next, next))
 
-  print(pcall(EXPECT_THAT, {1, 2, 3}, Not(Listwise(function(v) return Not(Equals(v)) end, {2, 4, 65}))))
+  EXPECT_FALSE(pcall(EXPECT_THAT, {1, 2, 3}, Not(Listwise(function(v) return Not(Equals(v)) end, {2, 4, 65}))))
+
+  EXPECT_FALSE(pcall(EXPECT_THAT, {7, 8, 9},
+                           Listwise(function(v) return GreaterThan(v) end,
+                                    {12, 2, 3})))
+
+  EXPECT_FALSE(pcall(EXPECT_THAT, {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+                           Listwise(function(v) return Listwise(GreaterThanOrEqual, v) end,
+                                    {{1, 2, 3}, {4, 5, 6}, {7, 8, 10}})))
+
 end
+
+
 
 test()
