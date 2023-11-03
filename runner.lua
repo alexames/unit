8a766dd22a7ec955501bb180c320a8caadd6d2bf
Alex Ames <alex@agentic.ai>
2023-11-02 23:51:01 -0700

    Replaced test_suites list with a filter

--- runner.lua
+++ runner.lua
@@ -12,18 +12,20 @@ function test_class(name)
   end
 end
 
-function run_unit_tests(test_suites, logger)
-  test_suites = test_suites or global_test_suites
+function run_unit_tests(filter, logger)
+  local test_suites = global_test_suites
   logger = logger or test_logger.TestLogger()
   local total_failure_count = 0
   local total_test_count = 0
   local failure_list = Table()
   logger.prelude()
   for _, cls in ipairs(test_suites) do
-    local test_object = cls()
-    local failed_tests, tests_ran = test_object:run_tests(logger)
-    total_failure_count = total_failure_count + failed_tests
-    total_test_count = total_test_count + tests_ran
+    if not filter or cls.__name:match(filter) then
+      local test_object = cls()
+      local failed_tests, tests_ran = test_object:run_tests(logger)
+      total_failure_count = total_failure_count + failed_tests
+      total_test_count = total_test_count + tests_ran
+    end
   end
   logger.finale(total_failure_count, total_test_count)
 end
