2cae3182de10d7b9917f22ca91d7ee198e64e289
Alexander Ames <Alexander.Ames@gmail.com>
2023-11-25 00:16:01 -0800

    Added EXPECT_TRUTHY, EXPECT_FALSEY, and EXPECT_ERROR.
    I'm not convinced the level handling is right though, that ought to be
    checked.

--- expects.lua
+++ expects.lua
@@ -14,19 +14,34 @@ function EXPECT_TRUE(actual, level)
   EXPECT_THAT(actual, Equals(true), level)
 end
 
-function EXPECT_FALSE(actual)
+function EXPECT_FALSE(actual, level)
   level = level or 3
   EXPECT_THAT(actual, Equals(false), level)
 end
 
-function EXPECT_EQ(actual, expected)
+function EXPECT_EQ(actual, expected, level)
   level = level or 3
   EXPECT_THAT(actual, Equals(expected), level)
 end
 
-function EXPECT_NE(actual, expected)
+function EXPECT_NE(actual, expected, level)
   level = level or 3
   EXPECT_THAT(actual, Not(Equals(expected)), level)
+end
+
+function EXPECT_TRUTHY(value)
+  level = level or 3
+  EXPECT_TRUE(truthy(value), level)
+end
+
+function EXPECT_FALSEY(value)
+  level = level or 3
+  EXPECT_FALSE(truthy(value), level)
+end
+
+function EXPECT_ERROR(fn, ...)
+  local successful, exception = pcall(fn, ...)
+  EXPECT_FALSEY(success, level)
 end
 
 return {
